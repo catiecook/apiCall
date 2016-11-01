22683293daa50d946a9a3963dc44b5fd2c06f662
catiecook <catiecook888@gmail.com>
2016-11-01 10:08:22 -0600

    pfdsafdsafdsa

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, ’10.0’
+
+target 'spot' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+	 pod 'Alamofire', '~> 4.0'
+  # Pods for spot
+	
+  target 'spotTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'spotUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- spot/ViewController.swift
+++ spot/ViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class TableViewController: UITableViewController {
+    
+    var names = [String]()
+    var searchURL = "https://api.spotify.com/v1/search?q=Alabama+Shakes&type=track&offset=20"
+    typealias JSONStandard = [String: AnyObject]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchURL)
     }
+    
+    func callAlamo(url: String){
+        Alamofire.request(url).responseJSON(completionHandler : {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData : Data) {
+        do {
+            let readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard {
+                if let items = tracks["items"] {
+                    
+                    for i in 0..<items.count {
+                        let item = items[i] as! JSONStandard
+                        let name = item["name"] as! String
+                        
+                        names.append(name)
+                    }
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
